f2afc57e4171129c849b2f356bf31b61ff431a1e
Sam Burden <sburden@uw.edu>
2023-09-24 06:29:29 -0700

    init

---
+++ _447.py
@@ -0,0 +1,111 @@
+# numpy = numerical Python, implements arrays (/ matrices)
+import numpy as np
+# limit number of decimal places printed for floating-point numbers
+np.set_printoptions(precision=3)
+
+# scipy = scientific Python, implements operations on arrays / matrices
+import scipy as sp
+# linalg = linear algebra, implements eigenvalues, matrix inverse, etc
+from scipy import linalg as la
+# optimize = optimization, root finding, etc
+from scipy import optimize as op
+# signal processing = filters, LTI systems
+from scipy import signal as sig
+
+# produce matlab-style plots
+import matplotlib as mpl
+# increase font size on plots
+mpl.rc('font',**{'size':18})
+# use LaTeX to render symbols
+mpl.rc('text',usetex=False)
+# render animation
+mpl.rc('animation',html='html5')
+# animation
+from matplotlib import animation as ani
+# Matlab-style plotting
+import matplotlib.pyplot as plt
+
+# symbolic computation, i.e. computer algebra (like Mathematica, Wolfram Alpha)
+import sympy as sym
+
+sym.init_printing(use_latex='mathjax')
+
+def Jacobian(g,y,d=1e-4):
+  """
+  approximate derivative via finite-central-differences
+
+  input:
+    g - function - g : R^n -> R^m
+    y - n array
+    (optional)
+    d - scalar - finite differences displacement parameter
+
+  output:
+    Dg(y) - m x n - approximation of Jacobian of g at y
+  """
+  # given $g:\mathbb{R}^n\rightarrow\mathbb{R}^m$:
+  # $$D_y g(y)e_j \approx \frac{1}{2\delta}(g(y+\delta e_j) - g(y - \delta e_j)),\ \delta\ll 1$$
+  e = np.identity(len(y))
+  Dyg = []
+  for j in range(len(y)):
+      Dyg.append((.5/d)*(g(y+d*e[j]) - g(y-d*e[j])))
+  return np.array(Dyg).T
+
+def numerical_simulation(f,t,x,t0=0.,dt=1e-4,ut=None,ux=None,utx=None,return_u=False):
+  """
+  simulate x' = f(x,u)
+
+  input:
+    f : R x X x U --> X - vector field
+      X - state space (must be vector space)
+      U - control input set
+    t - scalar - final simulation time
+    x - initial condition; element of X
+
+    (optional:)
+    t0 - scalar - initial simulation time
+    dt - scalar - stepsize parameter
+    return_u - bool - whether to return u_
+
+    (only one of:)
+    ut : R --> U
+    ux : X --> U
+    utx : R x X --> U
+
+  output:
+    t_ - N array - time trajectory
+    x_ - N x X array - state trajectory
+    (if return_u:)
+    u_ - N x U array - state trajectory
+  """
+  t_,x_,u_ = [t0],[x],[]
+
+  inputs = sum([1 if u is not None else 0 for u in [ut,ux,utx]])
+  assert inputs <= 1, "more than one of ut,ux,utx defined"
+
+  if inputs == 0:
+    assert not return_u, "no input supplied"
+  else:
+    if ut is not None:
+      u = lambda t,x : ut(t)
+    elif ux is not None:
+      u = lambda t,x : ux(x)
+    elif utx is not None:
+      u = lambda t,x : utx(t,x)
+
+  while t_[-1]+dt < t:
+    if inputs == 0:
+      _t,_x = t_[-1],x_[-1]
+      dx = f(t_[-1],x_[-1]) * dt
+    else:
+      _t,_x,_u = t_[-1],x_[-1],u(t_[-1],x_[-1])
+      dx = f(_t,_x,_u) * dt
+      u_.append( _u )
+
+    x_.append( _x + dx )
+    t_.append( _t + dt )
+
+  if return_u:
+    return np.asarray(t_),np.asarray(x_),np.asarray(u_)
+  else:
+    return np.asarray(t_),np.asarray(x_)
